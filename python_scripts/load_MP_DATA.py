266a4d003c7be66d4d19cd35629642aab1b9cdb0
AaronLge <lange@j-b-o.de>
2025-04-25 13:41:00 +0200

    database loading runs

---
+++ python_scripts/load_MP_DATA.py
@@ -0,0 +1,22 @@
+import xlwings as xw
+from db_loading import load_db_table
+import excel as ex
+
+
+def main(Structure_name, db_path):
+    logger = ex.setup_logger()
+
+    sheet_name_structure_loading = "BuildYourStructure"
+
+    META = load_db_table(db_path, "META")
+    DATA = load_db_table(db_path, Structure_name)
+
+    META_relevant = META.loc[META["table_name"] == Structure_name]
+   # ex.write_df_to_existing_table("GeometrieConverter.xlsm", sheet_name_structure_loading, "MP_Data", META_relevant)
+
+    ex.write_df("GeometrieConverter.xlsm", sheet_name_structure_loading, "Table_MP_META", META_relevant)
+    ex.write_df("GeometrieConverter.xlsm", sheet_name_structure_loading, "Table_MP_DATA", DATA)
+
+
+
+    return
